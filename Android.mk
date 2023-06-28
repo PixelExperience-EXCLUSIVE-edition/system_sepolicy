ca918e0a3952d4185aedb2623102c1f8feafada9
ARINDAM BHATTACHARJEE <abhattacharjee717@gmail.com>
2023-06-29 03:37:31 +0530

    sepolicy: Nuke blockage in user build for SELINUX_NEVER_ALLOW=true
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

--- Android.mk
+++ Android.mk
@@ -79,17 +79,6 @@ else
 mixed_sepolicy_build :=
 endif
 
-NEVERALLOW_ARG :=
-ifeq ($(SELINUX_IGNORE_NEVERALLOWS),true)
-ifeq ($(TARGET_BUILD_VARIANT),user)
-$(error SELINUX_IGNORE_NEVERALLOWS := true cannot be used in user builds)
-endif
-$(warning Be careful when using the SELINUX_IGNORE_NEVERALLOWS flag. \
-          It does not work in user builds and using it will \
-          not stop you from failing CTS.)
-NEVERALLOW_ARG := -N
-endif
-
 # BOARD_SEPOLICY_DIRS was used for vendor/odm sepolicy customization before.
 # It has been replaced by BOARD_VENDOR_SEPOLICY_DIRS (mandatory) and
 # BOARD_ODM_SEPOLICY_DIRS (optional). BOARD_SEPOLICY_DIRS is still allowed for
